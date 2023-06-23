995ed0381473bb1fb725dc6178f561c5fec618d9
Liza <liza.yemelyanova@list.ru>
2023-06-23 16:23:45 +0300

    add content loading to the sign page

--- src/js/sign.js
+++ src/js/sign.js
@@ -1,8 +1,28 @@
-// const header = new URL(document.location).searchParams,
-//       name = document.
+const header = new URL(document.location).searchParams,
+  sign_name = document.getElementById("sign_name"),
+  sign_date = document.getElementById("sign_date"),
+  sign_img = document.getElementById("sign_img"),
+  constellation = document.getElementById("constellation_img"),
+  info = document.getElementById("sign_info"),
+  famous = document.getElementById("famous"),
+  women = document.getElementById("famous_women"),
+  men = document.getElementById("famous_men");
 
-// if (header.get("sign") !== "404") {
-//   info.innerHTML += `${header.get("info")}`;
-// } else if (header.get("info") === "404") {
-//   info.innerHTML = "Ошибка 404! Данной страницы не существует";
-// }
+fetch("../signs.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    data.forEach(line => {
+        if (line.sign == header.get("sign")) {
+            sign_name.innerHTML = `${line.sign}`;
+            sign_date.innerHTML = `${line.date}`;
+            sign_img.src = `${line.img}`;
+            constellation.src = `${line.constellation}`;
+            info.innerHTML = `${line.characteristic}`;
+            famous.innerHTML += `${line.famous}`;
+            women.innerHTML = `${line.women}`;
+            men.innerHTML = `${line.men}`;
+        }
+    })
+  });
