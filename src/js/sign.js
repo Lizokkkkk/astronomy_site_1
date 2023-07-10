8da35d05324a8d8fd9e318ad4bed94694410cd4b
Liza <liza.yemelyanova@list.ru>
2023-07-10 20:42:35 +0300

    mod search.js

--- src/js/sign.js
+++ src/js/sign.js
@@ -1,28 +1,116 @@
-const header = new URL(document.location).searchParams,
-  sign_name = document.getElementById("sign_name"),
-  sign_date = document.getElementById("sign_date"),
-  sign_img = document.getElementById("sign_img"),
-  constellation = document.getElementById("constellation_img"),
-  info = document.getElementById("sign_info"),
-  famous = document.getElementById("famous"),
-  women = document.getElementById("famous_women"),
-  men = document.getElementById("famous_men");
+const data = document.getElementById('search_input'),
+      btn_horoscope = document.getElementById('search_btn1'),
+      btn_sign = document.getElementById('search_btn2'),
+      btn_planet = document.getElementById('search_btn3'),
+      btn_numerology = document.getElementById('search_btn4'),
+      btn_tarot = document.getElementById('search_btn5');
 
-fetch('/static/json/signs.json')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    data.forEach(line => {
-        if (line.sign == header.get("sign")) {
-            sign_name.innerHTML = `${line.sign}`;
-            sign_date.innerHTML = `${line.date}`;
-            sign_img.src = `${line.img}`;
-            constellation.src = `${line.constellation}`;
-            info.innerHTML = `${line.characteristic}`;
-            famous.innerHTML += `${line.famous}`;
-            women.innerHTML = `${line.women}`;
-            men.innerHTML = `${line.men}`;
-        }
-    })
-  });
+btn_horoscope.addEventListener("click", () => {
+    definitionSign();
+    location.href += `horoscope/?sign=${sign}`
+});
+
+btn_sign.addEventListener("click", () => {
+    definitionSign();
+    location.href += `sign/?sign=${sign}`
+});
+
+btn_planet.addEventListener("click", () => {
+    definitionSign();
+    location.href += `planet/?sign=${sign}`
+});
+
+btn_numerology.addEventListener("click", () => {
+    date = new Date(data.value);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    newDate = `${day}${month}${year}`
+    console.log(newDate)
+    location.href += `numerology/?date=${newDate}`
+})
+
+btn_tarot.addEventListener("click", () => {
+    definitionSign();
+    location.href += `tarot/?sign=${sign}`
+});
+
+definitionSign = () => {
+    birthdate = new Date(data.value);
+    const month = birthdate.getMonth() + 1;
+    const day = birthdate.getDate();
+    switch (month) {
+        case 1:
+            if (day <= 20)
+                sign = 'Козерог';
+            else
+                sign = 'Водолей';
+            break;
+        case 2:
+            if (day <= 18)
+                sign = 'Водолей';
+            else
+                sign = 'Рыбы';
+            break;
+        case 3:
+            if (day <= 20)
+                sign = 'Рыбы';
+            else
+                sign = 'Овен';
+            break;
+        case 4:
+            if (day <= 20)
+                sign = 'Овен';
+            else
+                sign = 'Телец';
+            break;
+        case 5:
+            if (day <= 20)
+                sign = 'Телец';
+            else
+                sign = 'Близнецы';
+            break;
+        case 6:
+            if (day <= 21)
+                sign = 'Близнецы';
+            else
+                sign = 'Рак';
+            break;
+        case 7:
+            if (day <= 22)
+                sign = 'Рак';
+            else
+                sign = 'Лев';
+            break;
+        case 8:
+            if (day <= 22)
+                sign = 'Лев';
+            else
+                sign = 'Дева';
+            break;
+        case 9:
+            if (day <= 23)
+                sign = 'Дева';
+            else
+                sign = 'Весы';
+            break;
+        case 10:
+            if (day <= 23)
+                sign = 'Весы';
+            else
+                sign = 'Скорпион';
+            break;
+        case 11:
+            if (day <= 22)
+                sign = 'Скорпион';
+            else
+                sign = 'Стрелец';
+            break;
+        case 12:
+            if (day <= 21)
+                sign = 'Стрелец';
+            else
+                sign = 'Козерог';
+            break;
+    }
+}
